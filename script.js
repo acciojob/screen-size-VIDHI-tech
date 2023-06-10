32e6a4987d2665e9f66ef58527b7558280590ae9
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-10 12:21:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-//your JS code here. If required.
+// Set the initial width and height values
+var initialWidth = window.innerWidth;
+var initialHeight = window.innerHeight;
+
+// Update the content of the <h1> tag
+var sizeInfo = document.getElementById("sizeInfo");
+sizeInfo.textContent = "Width: " + initialWidth + "px, Height: " + initialHeight + "px";
+
 window.addEventListener("resize", function() {
-  // Get the width and height of the window
+  // Get the updated width and height of the window
   var width = window.innerWidth;
   var height = window.innerHeight;
 
   // Update the content of the <h1> tag
-  var sizeInfo = document.getElementById("sizeInfo");
-  sizeInfo.textContent = "Width: " + width +" and "+ "Height: " + height ;
+  sizeInfo.textContent = "Width: " + width + "px, Height: " + height + "px";
 });
