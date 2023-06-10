1a55334e26bab12270ff597c86134345ca53e633
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-10 12:18:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ window.addEventListener("resize", function() {
 
   // Update the content of the <h1> tag
   var sizeInfo = document.getElementById("sizeInfo");
-  sizeInfo.textContent = "Width: " + width +" and"+ "Height: " + height ;
+  sizeInfo.textContent = "Width: " + width +" and "+ "Height: " + height ;
 });
