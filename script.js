9142460e6be30dbf1077dc2d12fbc18480d5dba0
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-10 12:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+window.addEventListener("resize", function() {
+  // Get the width and height of the window
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  // Update the content of the <h1> tag
+  var sizeInfo = document.getElementById("sizeInfo");
+  sizeInfo.textContent = "Width: " + width + "px, Height: " + height + "px";
+});
